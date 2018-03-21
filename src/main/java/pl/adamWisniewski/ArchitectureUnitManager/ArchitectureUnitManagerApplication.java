1b149d421d18ccc14f5f68d3ccb1e3f1d92df677
adam_wisniewski <awisniewski27@gmail.com>
2018-03-21 10:13:19 +0100

    initial commit

---
+++ src/main/java/pl/adamWisniewski/ArchitectureUnitManager/ArchitectureUnitManagerApplication.java
@@ -0,0 +1,12 @@
+package pl.adamWisniewski.ArchitectureUnitManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureUnitManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureUnitManagerApplication.class, args);
+	}
+}
